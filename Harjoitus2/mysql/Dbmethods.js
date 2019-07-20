3291d514507b39dc0b0bd225bfaaa40b2203f002
meincrab <meincrab@gmail.com>
2019-07-20 21:27:23 +0300

    mysql continues

---
+++ Harjoitus2/mysql/Dbmethods.js
@@ -0,0 +1,14 @@
+const conn = require('./dbconnection');
+
+const Dbmethods = {
+    /*metodi on esitetty oliosyntaksilla eli se on olion sisältämä avain:arvo -pari.
+    callback on anonyymi funktio jolla käsitellään kyselyn tulos. Se luodaan
+    tiedostoon jossa tämä metodi suoritetaan (add.js)*/
+
+    add: function (student_code, name, email, study_points, callback) {
+        return conn.query('insert into Students set student_code = ?, name = ?, email = ?, study_points = ?',
+            [student_code, name, email, study_points], callback);
+    }
+    // Tee tähän muut metodit
+};
+module.exports = Dbmethods;

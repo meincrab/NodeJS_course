ad9a53350d8bbedb5c8675c07c193489c6e37de7
meincrab <meincrab@gmail.com>
2019-07-21 21:49:47 +0300

    add.js added

---
+++ Harjoitus2/mysql/add.js
@@ -0,0 +1,17 @@
+/*
+ *  add.js lisää uuden Opiskelijan kantaan
+ */
+const Dbmethods = require('./Dbmethods');
+
+function handleError(err) {
+    console.error(err);
+    process.exit(1);
+}
+// add-metodin viimeisenä parametrina on callback, jolla saadaan kyselyn tulos
+// tässä tapauksessa tulos kertoo vain lisäyksen onnistumisen eli yksi tietue lisätty.
+Dbmethods.add('a1234', 'Ossi Opiskelija', 'a1234@jamk.fi', 105, function(err, result) {
+    if (err) {
+        return handleError(err);
+    }
+    console.log(result.affectedRows + ' records inserted' );
+});

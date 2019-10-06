5c085d0a7a8ee937be304fe7c269ffc9e56c172c
meincrab <meincrab@gmail.com>
2019-10-07 02:55:16 +0300

    User schema addded

---
+++ Harjoitus6/restapi/app/models/User.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+
+const UserSchema = new mongoose.Schema({
+    username: {type: String, unique: true, required: true},
+    password: {type: String, required: true},
+    isadmin: {type: Boolean, required: true}
+});
+
+const User = mongoose.model('User', UserSchema);

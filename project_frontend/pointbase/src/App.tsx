e69166f6e4bed46ac422d10421ce37a78ab41899
Meincrab <meincrab@gmail.com>
2019-10-10 02:52:17 +0300

    Started with course-assignment

---
+++ project_frontend/pointbase/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import AddNew from './modules/addNew';
+
+const App: React.FC = () => {
+  return (
+    <AddNew></AddNew>
+  );
+}
+
+export default App;
---
+++ project_frontend/pointbase/src/modules/addNew.tsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+
+class AddNew extends Component{
+
+render() {
+    return(
+        <h1>Yay, at least something I still remember :D</h1>
+        );
+    }
+}
+export default AddNew

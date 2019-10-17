5f598ed7f6b2691d90ca06e1c653517ebf998c42
Meincrab <meincrab@gmail.com>
2019-10-17 11:40:22 +0300

    Introduction to socket.io - done!

--- Harjoitus9/server.js
+++ Harjoitus9/server.js
@@ -6,12 +6,25 @@
  * Projektiin pitää asentaa socket.io: npm install socket.io
  */
 
-var http = require('http');
-var fs = require('fs');
-var Moniker = require('moniker');
-var users = [];
-var addUser = () => {
-    var user = {
+let http = require('http');
+let fs = require('fs');
+let Moniker = require('moniker');
+let users = [];
+
+
+let randomNum = (min, max) => {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+  }
+
+let randNumFirst = randomNum(0, 100000);
+let randNumSecond = randomNum(0, 100000);
+let thisGameRand = randNumFirst > randNumSecond ? randomNum(randNumSecond , randNumFirst) : randomNum(randNumFirst, randNumSecond);
+let numbers = [randNumFirst, randNumSecond];
+console.log(thisGameRand);
+let addUser = () =>{
+    let user = {
         name: Moniker.choose(),
         attempts: 0
     }
@@ -19,16 +32,29 @@ var addUser = () => {
     updateUsers();
     return user;
 }
-var updateUsers = function() {
-    var str = '';
-    for(var i=0; i<users.length; i++) {
-        var user = users[i];
-        str += user.name + ' <small>(' + user.clicks + ' clicks)</small><br />';
+
+let removeUser = (user) => {
+    for(let i=0; i<users.length;i++){
+        if(user.name === users[i].name) {
+            users.splice(i,1);
+            updateUsers();
+            return;
+        }
+    }
+}
+
+let updateUsers = () => {
+    let str = '';
+    for (let i=0; i< users.length; i++) {
+        let user = users[i];
+        str += user.name + ' <small>(' + user.attempts + ' attempts)</small><br />';
     }
     io.sockets.emit("users", { users: str });
 }
+
+
 //http-serveri joka laitetaan muuttujaan app tuottaa sivun client.html
-var app = http.createServer(function (req, res) {
+let app = http.createServer(function (req, res) {
     fs.readFile("client.html", 'utf-8', function (error, data) {
         res.writeHead(200, {'Content-Type': 'text/html'});
         res.write(data);
@@ -38,14 +64,16 @@ var app = http.createServer(function (req, res) {
 console.log('Http server in port 3010');
  
 //Socket-serveri kuuntelee http-serveriä
-var io = require('socket.io').listen(app);
+let io = require('socket.io').listen(app);
  
 //'connection'-tapahtuma suoritetaan joka kerta kun joku clientin 
 //socket yhdistää serverin socket.io moduliin. Parametrina
 //oleva muuttuja socket on viittaus clientin socketiin
 io.sockets.on('connection', function(socket) {
-    var user = addUser();
-    socket.emit("Welcome", user);
+    let user = addUser();
+    console.log(user);
+    socket.emit('welcome', user);
+    socket.emit('numbers', numbers)
     socket.on('disconnect', function () {
         removeUser(user);
     });
@@ -53,7 +81,23 @@ io.sockets.on('connection', function(socket) {
     socket.on('message_to_server', function(data) {
         //Lähetetään tullut data takaisin kaikille clientin socketeille
         //emitoimalla tapahtuma 'message_to_client' jolla lähtee JSON-dataa
-        io.sockets.emit("message_to_client",{ message: data["message"] });
+        console.log(data.message);
+        if(!Number.isInteger(Number.parseInt(data.message))){
+            console.log("not a number");
+            io.sockets.emit("message_to_client",{ message: "Not a number or not an integer, sorry" });
+        }
+        else{ 
+            if(thisGameRand < (Number.parseInt(data.message))){
+                io.sockets.emit("message_to_client",{ message: data["message"] + " is too big" });
+            }
+            else if(thisGameRand > (Number.parseInt(data.message))) { 
+                io.sockets.emit("message_to_client",{ message: data["message"] + " is too small" });
+            }
+            else {
+                io.sockets.emit("message_to_client", { message:data["message"] + " This is a Winner number!" });
+            }
+            
+        }   
+        updateUsers();
     });
 });
-
